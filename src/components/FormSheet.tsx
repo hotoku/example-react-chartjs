f05273910ebf3547c546dec53755225a3f828dd6
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-04 08:34:57 +0900

    Download button works.

--- src/components/FormSheet.tsx
+++ src/components/FormSheet.tsx
@@ -1,11 +1,33 @@
-import { Paper, Box } from "@mui/material";
+import { Paper, Box, Button } from "@mui/material";
+import { useSetAtom } from "jotai";
+import { MouseEventHandler, useState } from "react";
+import { download } from "../api";
+import { asyncValues } from "../atoms";
 
 import "./FormSheet.css";
 
 function FormSheet(): JSX.Element {
+  const [loading, setLoading] = useState(false);
+  const setValues = useSetAtom(asyncValues);
+
+  const clickHandler: MouseEventHandler = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setValues(
+      download().then((v) => {
+        setLoading(false);
+        return v;
+      })
+    );
+  };
+
   return (
     <Paper>
-      <Box className="form-base">form</Box>
+      <Box className="form-base">
+        <Button variant="contained" disabled={loading} onClick={clickHandler}>
+          download
+        </Button>
+      </Box>
     </Paper>
   );
 }
