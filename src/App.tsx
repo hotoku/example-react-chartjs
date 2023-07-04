a87167d9094fdcf24a3e36003bd795b6ea496358
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-04 09:08:34 +0900

    Introduce error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Box, Paper } from "@mui/material";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import "./App.css";
 import ChartSheet from "./components/ChartSheet";
 import FormSheet from "./components/FormSheet";
@@ -8,12 +9,14 @@ function App() {
   return (
     <Box className="App">
       <Paper elevation={3}>
-        <Box className="content-base">
-          <FormSheet />
-          <Suspense fallback={<Box>loading ...</Box>}>
-            <ChartSheet />
-          </Suspense>
-        </Box>
+        <ErrorBoundary fallback={<Box></Box>}>
+          <Box className="content-base">
+            <FormSheet />
+            <Suspense fallback={<Box>loading ...</Box>}>
+              <ChartSheet />
+            </Suspense>
+          </Box>
+        </ErrorBoundary>
       </Paper>
     </Box>
   );
